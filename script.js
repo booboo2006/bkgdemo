bb3818cc61eed3228f05a7ee287dbd3020614b7f
booboo2006 <diligentmedia2010@gmail.com>
2025-07-11 12:29:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").addEventListener("click", () => {
-  fetch("https://script.google.com/macros/s/AKfycbxlD9vTQavYJl5ZVHqkDV6kp1fhjYP_6eeht4e_NvgGafhPM6pJvcJkbheeHMcMv4-e/exec")
+  fetch("https://script.google.com/macros/s/AKfycbw7fVpWPI4vEedF7-GyImDV7fjpxYQ4ghZAMFBZz_gEzJqmkUhsUhiCh0cfPaj9wf4/exec")
     .then(response => response.json())
     .then(data => {
       document.getElementById("output").textContent = JSON.stringify(data, null, 2);
