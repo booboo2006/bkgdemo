6f5c8e26fede51fea4327bbd0664d4c95a40bf70
booboo2006 <diligentmedia2010@gmail.com>
2025-07-11 13:03:07 +0700

    Update and rename proxy.js to xxx

--- proxy.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://script.google.com/macros/s/AKfycbwJjfP22vyX8gnazhkH4wt7_30FRIkJUlDfIG-8fkUvIbiACkCgUUTRq4BwEAbTsTL0/exec");
-  const data = await response.json();
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data);
-}
